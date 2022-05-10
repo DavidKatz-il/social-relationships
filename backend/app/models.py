3ca6fac1e44378864cf5d513d8ecc84020aa4a9e
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2022-05-10 22:21:31 +0300

    small fix

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,6 +6,9 @@ from passlib.hash import bcrypt
 from app import database
 
 
+USERS_ID = "users.id"
+
+
 class User(database.Base):
     __tablename__ = "users"
     
@@ -27,7 +30,7 @@ class Person(database.Base):
     __table_args__ = (sql.UniqueConstraint("name", "owner_id"),)
 
     id = sql.Column(sql.Integer, primary_key=True, index=True)
-    owner_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
+    owner_id = sql.Column(sql.Integer, sql.ForeignKey(USERS_ID))
     
     name = sql.Column(sql.String, index=True)
     images = sql.Column(sql.String)
@@ -43,7 +46,7 @@ class Image(database.Base):
     __table_args__ = (sql.UniqueConstraint("name", "owner_id"),)
 
     id = sql.Column(sql.Integer, primary_key=True, index=True)
-    owner_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
+    owner_id = sql.Column(sql.Integer, sql.ForeignKey(USERS_ID))
     
     name = sql.Column(sql.String, index=True)
     image = sql.Column(sql.String)
@@ -58,7 +61,7 @@ class FacePerson(database.Base):
     __tablename__ = "faces_persons"
 
     id = sql.Column(sql.Integer, primary_key=True, index=True)
-    owner_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
+    owner_id = sql.Column(sql.Integer, sql.ForeignKey(USERS_ID))
     
     name = sql.Column(sql.String, sql.ForeignKey("persons.name"))
     face_locations = sql.Column(sql.JSON)
@@ -74,8 +77,8 @@ class FaceImage(database.Base):
     __tablename__ = "faces_images"
 
     id = sql.Column(sql.Integer, primary_key=True, index=True)
-    owner_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
-
+    owner_id = sql.Column(sql.Integer, sql.ForeignKey(USERS_ID))
+    
     name = sql.Column(sql.String, sql.ForeignKey("images.name"))
     face_locations = sql.Column(sql.JSON)
     face_encodings = sql.Column(sql.JSON)
@@ -85,19 +88,3 @@ class FaceImage(database.Base):
     datetime_updated = sql.Column(sql.DateTime, default=datetime.utcnow)
 
     owner = sql.orm.relationship("User", back_populates="faces_images")
-
-
-class Reports(database.Base):
-    __tablename__ = "reports"
-    __table_args__ = (sql.UniqueConstraint("name", "owner_id"),)
-
-    id = sql.Column(sql.Integer, primary_key=True, index=True)
-    owner_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
-
-    name = sql.Column(sql.String, index=True)
-    info = sql.Column(sql.String)
-
-    datetime_created = sql.Column(sql.DateTime, default=datetime.utcnow)
-    datetime_updated = sql.Column(sql.DateTime, default=datetime.utcnow)
-
-    owner = sql.orm.relationship("User", back_populates="reports")
