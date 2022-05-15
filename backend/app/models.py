5bd7f82684b50c5c4a0ebec5f72a5ac76ca2aca2
yaakovdayan1 <yaakovdayan1@gmail.com>
2022-05-15 15:00:38 +0300

    add reports model

--- backend/app/models.py
+++ backend/app/models.py
@@ -75,7 +75,7 @@ class FaceImage(database.Base):
 
     id = sql.Column(sql.Integer, primary_key=True, index=True)
     owner_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
-    
+
     name = sql.Column(sql.String, sql.ForeignKey("images.name"))
     face_locations = sql.Column(sql.JSON)
     face_encodings = sql.Column(sql.JSON)
@@ -85,3 +85,19 @@ class FaceImage(database.Base):
     datetime_updated = sql.Column(sql.DateTime, default=datetime.utcnow)
 
     owner = sql.orm.relationship("User", back_populates="faces_images")
+
+
+class Reports(database.Base):
+    __tablename__ = "reports"
+    __table_args__ = (sql.UniqueConstraint("name", "owner_id"),)
+
+    id = sql.Column(sql.Integer, primary_key=True, index=True)
+    owner_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
+
+    name = sql.Column(sql.String, index=True)
+    info = sql.Column(sql.String)
+
+    datetime_created = sql.Column(sql.DateTime, default=datetime.utcnow)
+    datetime_updated = sql.Column(sql.DateTime, default=datetime.utcnow)
+
+    owner = sql.orm.relationship("User", back_populates="reports")
