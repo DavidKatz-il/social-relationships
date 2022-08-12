93aa80aac73d1769de53357012192ab5e78d5ed8
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2022-08-12 15:59:22 +0300

    update home page

--- frontend/src/components/Info/Home.jsx
+++ frontend/src/components/Info/Home.jsx
@@ -1,5 +1,34 @@
 import React from "react"
 
 export const Home = () => {
-    return <h1 className="title" style={{ textAlign: "center" }}>Home...</h1>
-}
+    return (
+        <div className="container">
+            <div className="columns">
+                <div className="column">
+                    <section className="hero is-info welcome is-small">
+                        <div className="hero-body">
+                            <div className="container">
+                                <h1 className="title"> Hello, TEACHER NAME from SCHOOL NAME. </h1>
+                                <h2 className="subtitle"> Some welcome message! </h2>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="info-tiles">
+                        <div className="tile is-parent has-text-centered">
+                            <article className="tile is-child box">
+                                <p className="title">4</p>
+                                <p className="subtitle">Students</p>
+                            </article>
+                        </div>
+                        <div className="tile is-parent has-text-centered">
+                            <article className="tile is-child box">
+                                <p className="title">12</p>
+                                <p className="subtitle">Images</p>
+                            </article>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    );
+};
