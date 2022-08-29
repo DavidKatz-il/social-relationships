d8e24bc525d8ca284794cce73c02668635568248
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2022-08-29 18:41:09 +0300

    fix-lint

--- backend/app/services.py
+++ backend/app/services.py
@@ -130,7 +130,9 @@ async def get_current_user(
         payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
         user = db.query(models.User).get(payload["id"])
     except:
-        raise fastapi.HTTPException(status_code=401, detail="Invalid Email or Password.")
+        raise fastapi.HTTPException(
+            status_code=401, detail="Invalid Email or Password."
+        )
 
     return schemas.User.from_orm(user)
 
